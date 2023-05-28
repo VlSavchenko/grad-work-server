15901d5d7997a702af14dc8aefe8b1f89077e471
VlSavchenko <vlad.saw4encko2011@gmail.com>
2023-05-28 11:14:05 +0300

    commit for netlify5

--- functions/index.js
+++ functions/index.js
@@ -58,83 +58,19 @@ app.post('/generate-pdf', async (req, res) => {
     res.status(500).send('Произошла ошибка при генерации PDF');
   }
 
-  // const pdfBuffer = await generatePDF();
-
-  // res.setHeader('Content-Type', 'application/pdf');
-  // res.setHeader('Content-Disposition', `attachment; filename=CV.pdf`);
-
-  // // Отправка PDF-файла в ответе
-  // res.send(pdfBuffer);
-
-
-
-  // // Сохраните файл на сервере
-  // const filePath = path.join('../../testCVs/', uniqueFileName); // Замените путь на путь к вашей директории
-  // // Ваш код для генерации файла и сохранения его по указанному пути
-
-  // // Отправьте пользователю ссылку на скачивание файла
-  // const downloadUrl = `http://localhost:3002/download-pdf?file=${uniqueFileName}`;
-  // res.json({ downloadUrl });
 });
 
+const handler = async (event, context) => {
+  const result = await app(event, context);
+  return result;
+};
 
-// app.get('/download-pdf', (req, res) => {
-//   const fileName = req.query.file; // Получите имя файла из параметра запроса
+module.exports = { handler };
 
-//   const filePath = path.join('../../testCVs/', fileName); // Замените путь на путь к вашей директории
-
-//   // Установка заголовков ответа для передачи файла
-//   res.setHeader('Content-Type', 'application/pdf');
-//   res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
-
-//   // Отправка файла в ответе
-//   res.sendFile(filePath);
-// });
 
 // Запускаем сервер и слушаем указанный порт
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
 
-// function generateUniqueFileName() {
-//   const uniqueId = v4(); // Генерируем уникальный идентификатор
-//   const fileName = `${uniqueId}.pdf`; // Формируем имя файла, например, с расширением .pdf
-//   return fileName;
-// }
 
-// aaa
-
-// async function generatePDF() {
-//   console.log('In generatePDF');
-//   const browser = await chrome.puppeteer.launch({
-//     args: chrome.args,
-//     defaultViewport: chrome.defaultViewport,
-//     executablePath: await chrome.executablePath,
-//     headless: "new",
-//     ignoreHTTPSErrors: true,
-//   }); //{headless:false}
-//   const page = await browser.newPage();
-//   console.log('after newPage');
-//   await page.setContent(CV5);
-//   console.log('after setContent');
-//   const pdfBuffer = await page.pdf({ format: 'A4' });
-//   console.log('after creating pdf');
-//   // await page.pdf({ path: '../../testCVs/cv5.pdf', format: 'A4' });
-//   await browser.close();
-//   console.log('after browser.close');
-//   return pdfBuffer;
-// }
-
-// async function generatePDF() {
-//   const browser = await puppeteer.launch(); //{headless:false}
-//   const page = await browser.newPage();
-//   await page.setContent(CV5);
-//   const pdfBuffer = await page.pdf({ format: 'A4' });
-//   // await page.pdf({ path: '../../testCVs/cv5.pdf', format: 'A4' });
-//   await browser.close();
-//   return pdfBuffer;
-// }
-
-// generatePDF().catch(error => {
-//   console.error('Ошибка при создании PDF:', error);
-// });
